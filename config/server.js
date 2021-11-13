b0bcbf02d3c5cd9e0fe0bfbc6dc2bcf6e6813061
Faizal Amiruddin <faizal@wowrack.com>
2021-11-13 12:17:27 +0700

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f0c8b3ced877e477dc26dad6207a0085'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e94622c7-7a1e-4aa7-a77f-30b89f1d6f64'
+};
